02577771b9c6a4c59c604bf0b5d0c7eecc01cbfc
olegsokolansky <oleg.sokolansky@gmail.com>
2018-06-18 07:27:40 +0300

    swipe epsilon

--- Wheel/GestureRecognizers/SWSwipeGestureRecognizer.swift
+++ Wheel/GestureRecognizers/SWSwipeGestureRecognizer.swift
@@ -1,142 +1,131 @@
 //
-//  SWSwipeGestureRecognizer.swift
+//  SWSwipeGestureRecognizer-2.swift
 //  Wheel
 //
-//  Created by Oleg Sokolansky on 03/05/2018.
+//  Created by Oleg Sokolansky on 10/06/2018.
 //  Copyright © 2018 Oleg Sokolansky. All rights reserved.
 //
-//
-//import Foundation
-//import UIKit
-//import UIKit.UIGestureRecognizerSubclass
-//
-//class SWSwipeGestureRecognizer: UIGestureRecognizer {
-//    
-//    private let pathToRecognize: CGFloat = 100
-//    
-//    private var initial: CGPoint = .zero
-//    
-//    private var isIdle: Bool = true
-//    
-//    var Initial: CGPoint {
-//        get {
-//            return initial
-//        }
-//    }
-//    
-//    private var current: CGPoint = .zero
-//    
-//    var Current: CGPoint {
-//        get {
-//            return current
-//        }
-//    }
-//    
-//    var Progress: CGFloat {
-//        get {
-//            return abs(current.x - initial.x > 0 ? current.x - initial.x : 0) / pathToRecognize
-//        }
-//    }
-//    
-//    var IsIdle: Bool {
-//        get {
-//            return isIdle
-//        }
-//    }
-//    
-//    var Delegate: SWSwipeDelegate?
-//    
-//    // MARK: - Overriden Methods
-//    
-//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
-//        super.touchesBegan(touches, with: event)
-//        isIdle = false
-//        initial = location(ofTouch: 0, in: view!)
+
+
+import Foundation
+import UIKit
+import UIKit.UIGestureRecognizerSubclass
+
+class SWSwipeGestureRecognizer: UIGestureRecognizer {
+    
+    private let path: CGFloat = UIScreen.main.bounds.size.width
+    
+    private let pathToBegin: CGFloat = 10
+    
+    private var IsInProgress: Bool = false
+    
+    private var initial: CGPoint = .zero
+    
+    var Initial: CGPoint {
+        get {
+            return initial
+        }
+    }
+    
+    private var current: CGPoint = .zero
+    
+    var Current: CGPoint {
+        get {
+            return current
+        }
+    }
+    
+    var ProgressToRecognize: CGFloat {
+        get {
+            return 0.5
+        }
+    }
+    
+    var Progress: CGFloat {
+        get {
+            return IsInProgress ? (abs(current.x - initial.x > 0 ? current.x - initial.x : 0) / path) : 0
+        }
+    }
+    
+    private var IsTooSmallToCheck: Bool {
+        get {
+            return abs(initial.x - current.x) < 10 && abs(initial.y - current.y) < 10
+        }
+    }
+    
+    private var IsRightDownDirection: Bool {
+        get {
+            let dy = (current.y - initial.y)
+            let dx = (current.x - initial.x)
+            return !(dy / dx > tan(CGFloat.pi * 0.33)) && dy > 0 && dx > 0
+        }
+    }
+    
+    var Delegate: SWSwipeDelegate?
+    
+    // MARK: - Overriden Methods
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        initial = location(ofTouch: 0, in: view!)
+        IsInProgress = false
 //        current = initial
 //        Delegate?.onBegin(self)
-//    }
-//    
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
-//        //super.touchesMoved(touches, with: event)
-//        let current = location(ofTouch: 0, in: view!)
-//        if self.initial == self.current {
-//            let direction = CGPoint(x: current.x - initial.x, y: current.y - initial.y)
-//            if direction.x < 0 {
-//                state = .failed
-//                Delegate?.onCancel(self)
-//                print("SWSwipe cancelled from \(initial) to \(current)")
-//            }
-//            else if direction.y < 0 {
-//                state = .failed
-//                Delegate?.onCancel(self)
-//                print("SWSwipe cancelled from \(initial) to \(current)")
-//            }
-//            else if direction.y / direction.x > tan(CGFloat.pi * 0.33) {
-//                state = .failed
-//                Delegate?.onCancel(self)
-//                print("SWSwipe cancelled from \(initial) to \(current) at angle \(atan(direction.y / direction.x) / CGFloat.pi) * pi")
-//            }
-//            else {
-//                print("SWSwipe ok from \(initial) to \(current)")
-//                self.current = current
-//                if Progress < 0 {
-//                    state = .failed
-//                    print("SWSwipe cancel")
-//                    Delegate?.onCancel(self)
-//                }
-//                else if Progress < 1 {
-//                    Delegate?.onMove(self)
-//                }
-//                else if Progress >= 1 {
-//                    print("SWSwipe finish")
-//                    Delegate?.onFinish(self, true)
-//                }
-//            }
-//        }
-//        else {
-//            self.current = current
-//            if Progress < 0 {
-//                state = .failed
-//                print("SWSwipe cancel")
-//                Delegate?.onCancel(self)
-//            }
-//            else if Progress < 1 {
+//        state = .began
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        self.current = location(ofTouch: 0, in: view!)
+        if IsTooSmallToCheck {
+            print("IsTooSmallToCheck")
+//            state = .changed
+        }
+        else if !IsInProgress && !IsRightDownDirection {
+            print("!IsInProgress && !IsRightDownDirection")
+            state = .cancelled
+            Delegate?.onCancel(self)
+        }
+        else if !IsInProgress && IsRightDownDirection {
+            print("!IsInProgress && IsRightDownDirection")
+            IsInProgress = true
+            state = .changed
+            Delegate?.onBegin(self)
+        }
+        else {
+            print("!IsTooSmallToCheck && IsInProgress")
+            if Progress < 0 {
+                state = .cancelled
+                print("SWSwipe cancel")
+                Delegate?.onCancel(self)
+            }
+            else {
+                state = .changed
+                Delegate?.onMove(self)
+            }
+//            else if Progress < ProgressToRecognize {
+//                state = .changed
 //                Delegate?.onMove(self)
 //            }
-//            else if Progress >= 1 {
+//            else if Progress >= ProgressToRecognize {
 //                print("SWSwipe finish")
+//                state = .changed
 //                Delegate?.onFinish(self, true)
 //            }
-//        }
-//        
-//    }
-//    
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
-//        //super.touchesEnded(touches, with: event)
-//        if current != initial && state == .possible {
-//            print("SWSwipe from \(initial) to \(current)")
-//            state = .recognized
-//            print("SWSwipe finish")
-//            Delegate?.onFinish(self, Progress >= 1)
-//        }
-//        else {
-//            state = .failed
-//            print("SWSwipe cancel")
-//            Delegate?.onCancel(self)
-//            isIdle = true
-//        }
-//    }
-//    
-//    // MARK: - Public Methods
-//    
-//    func forceCompletion() {
-//        
-//        state = .recognized
-//    }
-//    
-//    func forceCancel() {
-//        state = .failed
-//    }
-//    
-//}
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        //super.touchesEnded(touches, with: event)
+        if state != .cancelled && Progress >= ProgressToRecognize {
+            print("SWSwipe from \(initial) to \(current)")
+            state = .recognized
+            print("SWSwipe finish")
+            Delegate?.onFinish(self, Progress >= ProgressToRecognize)
+        }
+        else {
+            state = .cancelled
+            print("SWSwipe cancel")
+            Delegate?.onCancel(self)
+        }
+    }
+}
 
